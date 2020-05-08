6a73f5f3bc696c600312543969bad0aa800bd6f1
Machbase <machbase@machbase.com>
2020-05-08 11:02:50 +0900

    tagRepository import typo

--- sample/tag/src/main/java/com/machbase/tag/repository/TagRepository.java
+++ sample/tag/src/main/java/com/machbase/tag/repository/TagRepository.java
@@ -3,6 +3,11 @@ package com.machbase.tag.repository;
 import com.machbase.tag.domain.Tag;
 import com.machbase.tag.domain.TagPK;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 
 public interface TagRepository extends JpaRepository<Tag, TagPK> {
     @Query(value = "SELECT * FROM tag WHERE name = :name", nativeQuery=true)
